7beea1f786f6b4c69cc37846fd7ecf619948aca9
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2019-07-10 22:50:41 +0300

    chore(tsconfig): FIx type declarations

--- src/app.d.ts
+++
@@ -1,6 +0,0 @@
-import { Foo } from "./foo";
-export declare let y: {
-    some: string;
-};
-export declare const foo: Foo;
-export { Foo };
--- src/foo.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Foo {
-    private bar;
-    getBar: () => string;
-    setBar: (value: string) => void;
-}
