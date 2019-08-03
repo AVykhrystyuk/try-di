f82779b079d9f6af4eaf7858761a1bbb186170e2
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2019-08-04 00:13:40 +0300

    chore: add first tests for custom error

---
+++ src/interfaces/di-error.spec.ts
@@ -0,0 +1,34 @@
+import { describe, it } from 'mocha';
+import * as assert from 'assert';
+
+import { DependencyInjectionError } from './di-error';
+
+describe('DependencyInjectionError', () => {
+  it('is instance of Error', () => {
+    const error = new DependencyInjectionError('Error reason message');
+
+    assert.ok(error instanceof Error);
+  });
+
+  it('`.toString()` displays name and the message for a direct call', () => {
+    const error = new DependencyInjectionError('Error reason message');
+
+    assert.equal(error.toString(), 'DependencyInjectionError: Error reason message');
+  });
+
+  it('`.stack` displays name and the message and the stackTrace', function stackTraceTestCase() {
+    const error = new DependencyInjectionError('Error reason message');
+
+    const stackTrace = error.stack as string;
+    const stackTraceLines = stackTrace.split('\n');
+    assert.ok(stackTrace.startsWith('DependencyInjectionError: Error reason message'));
+    assert.ok(stackTraceLines.length > 5);
+    assert.equal(stackTraceLines[1].indexOf('stackTraceTestCase'), 15);
+  });
+
+  it('Does not have enumerable properties - otherwise they will be displayed in `console.log(err)`', () => {
+    const error = new DependencyInjectionError('Error reason message');
+
+    assert.equal(Object.keys(error).length, 0);
+  });
+});
