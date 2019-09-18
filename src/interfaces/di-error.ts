d6d1baa3db8003a83fe234832a34895af46f50e3
Alexey Vykhrystyuk <AVykhrystyuk@users.noreply.github.com>
2019-09-18 11:22:02 +0300

    refactor: innerError property is public in custom error

--- src/interfaces/di-error.ts
+++ src/interfaces/di-error.ts
@@ -1,11 +1,9 @@
 export class DependencyInjectionError extends Error {
-  public constructor(message: string, innerError?: Error) {
+  public constructor(message: string, public readonly innerError?: Error) {
     super(message);
 
     // to resist minification error name is hard coded
     defineProperty(this, 'name', 'DependencyInjectionError'); // this.constructor.name);
-
-    defineProperty(this, 'innerError', innerError);
   }
 }
 
