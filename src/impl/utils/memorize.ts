0fc3984b2fef69c1e14982340b5527164399a9fc
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2021-06-01 22:48:31 +0300

    refactor: add thisArg for memoize

--- src/impl/utils/memorize.ts
+++ src/impl/utils/memorize.ts
@@ -4,20 +4,24 @@ type FunctionLike = (...args: any) => any;
 
 type CacheHolder = { cache: Map<any, any> };
 
-export const memoize = <T extends FunctionLike>(func: T, resolver?: FunctionLike): T & CacheHolder => {
+export const memoize = <T extends FunctionLike>(
+  func: T,
+  thisArg?: unknown,
+  resolver?: FunctionLike
+): T & CacheHolder => {
   if (typeof func !== 'function' || (resolver != null && typeof resolver !== 'function')) {
     throw new TypeError('Expected a function');
   }
 
   const memoized = (...args: any[]) => {
-    const key = resolver ? resolver.apply(this, args) : args[0];
+    const key = resolver ? resolver.apply(thisArg, args) : args[0];
     const { cache } = memoized;
 
     if (cache.has(key)) {
       return cache.get(key);
     }
 
-    const result = func.apply(this, args);
+    const result = func.apply(thisArg, args);
     cache.set(key, result);
     return result;
   };
